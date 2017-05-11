988f487e21d88748f25637b3127ae3c24ff018cf
John Wu <me@johnwu.cc>
2017-05-11 09:59:30 +0800

    initial version

---
+++ MyWebsite/MyWebsite/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace MyWebsite
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
