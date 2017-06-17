d04ed56c2c75db07de2c779b664fc708834eae49
John Wu <me@johnwu.cc>
2017-06-16 23:00:39 -0700

    Add MVC

--- MyWebsite/MyWebsite/Startup.cs
+++ MyWebsite/MyWebsite/Startup.cs
@@ -1,16 +1,23 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MyWebsite
 {
     public class Startup
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
